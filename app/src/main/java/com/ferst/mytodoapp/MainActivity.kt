40f12e57489d32e122e9b00e2682c1cacf0add57
MateuszFerst <107873042+MateuszFerst@users.noreply.github.com>
2025-04-28 20:38:29 +0200

    MyTodoApp
    A mobile app for android as part of an engineering thesis

--- app/src/main/java/com/ferst/mytodoapp/MainActivity.kt
+++ app/src/main/java/com/ferst/mytodoapp/MainActivity.kt
@@ -34,9 +34,9 @@ class MainActivity : AppCompatActivity() {
     private val ALL_TASKS_ID = 1L // Stała identyfikująca "Wszystkie"
     private val taskListsCache = Collections.synchronizedList(mutableListOf<TaskList>()) // Przechowywanie list w pamięci
 
-    val MIGRATION_2_2 = object : Migration(2, 2) {
+    val MIGRATION_2_3 = object : Migration(2, 3) {
         override fun migrate(database: SupportSQLiteDatabase) {
-            database.execSQL("ALTER TABLE lists ADD COLUMN orderIndex INTEGER NOT NULL DEFAULT 0")
+            database.execSQL("ALTER TABLE lists ADD COLUMN taskCount INTEGER DEFAULT 0 NOT NULL")
         }
     }
 
@@ -49,7 +49,7 @@ class MainActivity : AppCompatActivity() {
             applicationContext,
             AppDatabase::class.java,
             "tasks.db"
-        ).addMigrations(MIGRATION_2_2) // Dodanie migracji
+        ).addMigrations(MIGRATION_2_3) // Dodanie migracji
             .build()
 
         // Sprawdzenie powtarzających się zadań
